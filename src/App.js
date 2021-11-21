8fd8eca15c6954b8ed0466877a9a4e0e294d7fb9
Prasundas99 <prasundas115@gmail.com>
2021-11-21 23:34:36 +0530

    Finalising application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
+
 function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -20,12 +23,13 @@ function App() {
       <ImageSearch searchText={(text) => setTerm(text)} />
 
       {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1> }
-
+      <div class="md:container md:mx-auto">
       {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
         {images.map(image => (
           <ImageCard key={image.id} image={image} />
         ))}
       </div>}
+      </div>
     </div>
   );
 }
